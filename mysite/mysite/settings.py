e423b9068eff40551c5adf862171707f59a11f12
Ana Lucía Dueñas Chávez <a01702080@itesm.mx>
2019-02-21 22:50:46 -0600

    Writing your first Django app, part 2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tutorial',
+        'USER': 'postgres',
+        'PASSWORD': 'analu250',
+        'HOST': '127.0.0.1',
+        'PORT': '8080',
     }
 }
 
